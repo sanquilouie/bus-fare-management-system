46ab37ea891b0ce583090b0f2809283a49391bf6
louieeeee <louiesanqui0518@gmail.com>
2025-04-06 15:40:32 +0800

    Bus Fare No Location Detected Bug

--- NewRam/pages/conductor/busfare.php
+++ NewRam/pages/conductor/busfare.php
@@ -476,6 +476,8 @@ function getBusLocation() {
                 .then(data => {
                     console.log("Response Data:", data);
                     if (data.error) {
+                        document.getElementById("fromRoute").innerText = JSON.stringify(data, null, 2);
+                        document.getElementById("fromRoute").style.display = 'none';
                         document.getElementById("displayRouteName").innerText = data.error;
                     } else {
                         document.getElementById("fromRoute").innerText = JSON.stringify(data, null, 2);
