8d243f31398d4a4c5dd6189772c13e217459fd2c
louieeeee <louiesanqui0518@gmail.com>
2025-04-07 15:41:04 +0800

    tt

--- NewRam/pages/conductor/busfare.php
+++ NewRam/pages/conductor/busfare.php
@@ -282,7 +282,7 @@ $conn->close();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Bus Fare Calculator</title>
+    <title>Bus Fare Calculatorrrrrr</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700,800,900">
