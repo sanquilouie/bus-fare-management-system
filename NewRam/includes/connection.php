b608335091059739fab0b63e3d00cef930ca6390
louieeeee <louiesanqui0518@gmail.com>
2025-05-03 07:59:19 +0800

    set time zone

--- NewRam/includes/connection.php
+++ NewRam/includes/connection.php
@@ -1,8 +1,4 @@
 <?php
-// connection.php
-
-// config.php
-
 if ($_SERVER['HTTP_HOST'] === 'localhost') {
     // Local
     $dbhost = 'localhost';
@@ -17,13 +13,13 @@ if ($_SERVER['HTTP_HOST'] === 'localhost') {
 	$dbname = 'REDACTED_DB_NAME';
 }
 
-// Mysqli connection
 $conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
 if (!$conn) {
 	die("Failed to connect using MySQLi: " . mysqli_connect_error());
 }
 
-// PDO connection
+mysqli_query($conn, "SET time_zone = 'Asia/Manila'");
+
 try {
 	$pdo = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass);
 	// Set the PDO error mode to exception
