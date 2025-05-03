be42ff72771548d81e1b2ca217d6900d7ba8b259
louieeeee <louiesanqui0518@gmail.com>
2025-05-03 09:07:05 +0800

    Fixed search error

--- NewRam/actions/search_users.php
+++ NewRam/actions/search_users.php
@@ -5,7 +5,7 @@ include "../includes/connection.php";
 if (isset($_POST['query'])) {
     $search = mysqli_real_escape_string($conn, $_POST['query']);
 
-    $searchQuery = "SELECT id, firstname, middlename, lastname, birthday, age, gender, address,province,municipality,barangay, account_number, balance 
+    $searchQuery = "SELECT id, firstname, middlename, lastname, birthday, age, gender, address,province,municipality,barangay, account_number, balance, status 
                     FROM useracc 
                     WHERE is_activated = 1 AND role = 'User'
                     AND (account_number LIKE '%$search%' OR email LIKE '%$search%')"; // Assuming you have an email column
@@ -14,6 +14,8 @@ if (isset($_POST['query'])) {
 
     if (mysqli_num_rows($result) > 0) {
         while ($row = mysqli_fetch_assoc($result)) {
+            $status = isset($row['is_activated']) ? ($row['is_activated'] == 1 ? 'Activated' : 'Disabled') : 'N/A';
+
             echo '<tr>
                     <td>' . $row['id'] . '</td>
                     <td>' . $row['firstname'] . '</td>
@@ -22,9 +24,9 @@ if (isset($_POST['query'])) {
                     <td>' . date('F j, Y', strtotime($row['birthday'])) . '</td>
                     <td>' . $row['age'] . '</td>
                     <td>' . $row['gender'] . '</td>
-                    <td>' . $row['address'] . '</td>
-                    <td>' . $row['account_number'] . '</td>
+                    <td>' . $row['account_number'] . '</td>  
                     <td>₱' . number_format($row['balance'], 2) . '</td>
+                    <td>' . $status . '</td>   
                 </tr>';
         }
     } else {
