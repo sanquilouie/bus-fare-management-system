0e716d2ad2ccb344b2005861dad40ac3c0f37352
louieeeee <louiesanqui0518@gmail.com>
2025-05-07 18:02:41 +0800

    Fixed remit logs

--- NewRam/pages/conductor/busfare_auto.php
+++ NewRam/pages/conductor/busfare_auto.php
@@ -481,7 +481,7 @@ $conn->close();
                 <div class="mb-2 text-start fw-bold">
                     <label for="directionDropdown" class="form-label">Direction</label>
                     <select class="form-select" id="directionDropdown">
-                        <option value="">Select Direction</option>
+                        <option value="">**Select Direction**</option>
                         <option value="East to West">East to West</option>
                         <option value="West to East">West to East</option>
                     </select>
